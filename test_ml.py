03b45aad4ec174fa6d19fa51b8e5cde048d3f300
mournhart <wcshek@SpeedyCrow.localdomain>
2026-05-23 15:53:20 -0700

    Added first unit test to test_ml.py

--- test_ml.py
+++ test_ml.py
@@ -1,13 +1,24 @@
 import pytest
 # TODO: add necessary import
+from sklearn.ensemble import RandomForestClassifier
 
 # TODO: implement the first test. Change the function name and input as needed
-def test_one():
+
+def test_model_used_is_rfc():
     """
-    # add description for the first test
+    # Test that the ML model used is RandomForestClassifier.
     """
     # Your code here
-    pass
+     X_train, y_train, encoder, lb = process_data(
+        train,
+        categorical_features=cat_features,
+        label="salary",
+        training=True
+    )
+
+    model = train_model(X_train, y_train)
+
+    assert isinstance(model, RandomForestClassifier)
 
 
 # TODO: implement the second test. Change the function name and input as needed
