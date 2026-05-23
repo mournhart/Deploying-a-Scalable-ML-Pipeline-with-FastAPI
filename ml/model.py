1d4b092296a60e030eddcd941a3b2af6faa5ae4c
mournhart <78252030+mournhart@users.noreply.github.com>
2026-05-22 23:42:11 -0700

    Update model.py
    removed unused imports.

--- ml/model.py
+++ ml/model.py
@@ -3,7 +3,7 @@ from sklearn.metrics import fbeta_score, precision_score, recall_score
 from ml.data import process_data
 # TODO: add necessary import
 from sklearn.ensemble import RandomForestClassifier 
-from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.model_selection import GridSearchCV
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
